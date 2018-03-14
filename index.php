204244965ee6115225f066210dc40874609208ea
pr04 <pr04@figd-1711704.figdtk.de>
2018-03-14 10:10:59 +0100

    Erste Version 14.03.2018

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP 01 Basic</title>
+        <title>PHP 03_CSV</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
