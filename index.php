3487a72dbb692f9fae8318420252b773367f8f5c
pr04 <pr04@figd-1711704.figdtk.de>
2018-03-14 09:24:56 +0100

    Erste Version 14.03.2018

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP 01 Basic</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+        <!-- Reihenfolge beachten: zuerst css - Bootstrap, dann eigene, 
+        dann JS JQuery, Bootstrap und eigene-->        
+    </head>
+    <body>
+        <label>CSV & Co</label>
+        <div class="container">
+            <div class="col-12"></div>
+            <?php
+                
+            ?>
+        </div>
+        <hr>
+        <pre>
+            <div class="ue1">
+            <?php 
+                
+            ?>
+            </div>
+        </pre> 
+            
+         
+        
+    </body>        
+</html>
